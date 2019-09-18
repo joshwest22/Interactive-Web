289907b85fafda712875db4fd596a9081a3f7d16
Josh West <josh.west@centre.edu>
2019-09-18 16:18:26 -0400

    added 3rd image and formatting changes

--- javascript.js
+++ javascript.js
@@ -1,7 +1,12 @@
 var switchImage = function()
 {
     var node = document.querySelector("#firstimage");
-    node.setAttribute("src","ispy1.jpg")
+    node.setAttribute("src","ispy2.jpg")
 }
+/*var switchImage2 = function()
+{
+    var node = document.querySelector("#firstimage");
+    node.setAttribute("src","ispy3.jpg")
+}*/
 console.log("JAVASCRIPT WORKING")
 //switchImage();
